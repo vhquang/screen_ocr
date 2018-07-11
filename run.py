10ee7c7f6fd107b892b8db03b425bf7bcab53e4c
Quang Vu <vhquang@gmail.com>
2018-07-11 13:50:38 -0700

    Refactor code

--- run.py
+++ run.py
@@ -26,59 +26,62 @@ class Stoppable_Thread (threading.Thread):
 
 
 class Capture_Windows ():
-    def __init__(self):
-        self.exitFlag = False
+    def __init__(self, width=600, height=400):
+        self._exit = False
+        self.high_alpha = 0.6
+        self.low_alpha = 0.07
         self.root = Tk()
+        self.root.wm_attributes("-topmost", 1)
+        self.root.geometry('{w}x{h}'.format(w=width, h=height))
+
         # put window's info at bottom of the window
         self.position_str = StringVar()
-        Message(self.root, textvariable=self.position_str, width=150).pack(side=tkinter.BOTTOM)
+        message = Message(self.root, textvariable=self.position_str, width=150)
+        message.pack(side=tkinter.BOTTOM)
 
-        self.root.wm_attributes("-topmost", 1)
-        self.root.geometry("600x400")
         self.root.attributes('-alpha', 0.1)
-        # increase window' opacity when mouse hover
         self.root.bind("<Enter>", self.mouseEnter)
-        # decrease window's opacity 2 seconds after mouse leave
         self.root.bind("<Leave>", self.mouseLeave)
-        self.root.after(200, self.output)
         self.root.protocol("WM_DELETE_WINDOW", self.on_quit)
-        self.root.mainloop()
-
-        # root.update()
-        # print (root.winfo_width())
-        # print (root.winfo_height())
-        # print (root.winfo_reqwidth())
 
     def increaseAlpha(self):
-        self.root.attributes('-alpha', 0.6)
+        self.root.attributes('-alpha', self.high_alpha)
 
     def decreaseAlpha(self):
-        # don't set attribute after the root been destroyed
-        if not self.exitFlag:
-            self.root.attributes('-alpha', 0.07)
+        if not self._exit:
+            self.root.attributes('-alpha', self.low_alpha)
 
     def mouseEnter(self, event):
+        """
+        Increase window' opacity when mouse hover.
+        """
         for thread in threading.enumerate():
             if isinstance(thread, Stoppable_Thread):
                 thread.stop()
         self.increaseAlpha()
 
     def mouseLeave(self, event):
+        """
+        Decrease window's opacity 2 seconds after mouse leave.
+        """
         thread = Stoppable_Thread(2, self.decreaseAlpha)
         thread.start()
 
     def on_quit(self):
-        self.exitFlag = True
+        self._exit = True
         self.root.destroy()
 
     def output(self):
+        # print (root.winfo_width())
+        # print (root.winfo_height())
+        # print (root.winfo_reqwidth())
         info_str = self.root.geometry()
         # width,height,posX,posY = [int(x) for x in re.split(r'[x+]', info_str)]
         posX = self.root.winfo_rootx()
         posY = self.root.winfo_rooty()
         width = self.root.winfo_width()
         height = self.root.winfo_height()
-        display_message = "X: %d, Y: %d, W: %d H: %d"%(posX, posY, width, height)
+        display_message = "X: %d, Y: %d \n W: %d H: %d"%(posX, posY, width, height)
         self.position_str.set( display_message )
 
         caputure_box = posX, posY, posX+width, posY+height-20
@@ -87,11 +90,16 @@ class Capture_Windows ():
 
         start = time.clock()
         # print(pytesser.image_to_string(img))
-        print(time.clock() - start)
+        # print(time.clock() - start)
 
+        if not self._exit:
+            self.root.after(200, self.output)
+
+    def run(self):
         self.root.after(200, self.output)
-
+        self.root.mainloop()
 
 
 if __name__ == '__main__':
     capture = Capture_Windows()
+    capture.run()
